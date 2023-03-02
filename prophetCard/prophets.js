d0960974df529a130b0d6cec75c10d62026e26d8
Teresa Moser <moserte@byui.edu>
2023-03-01 22:36:30 -0700

    add js file

---
+++ prophetCard/prophets.js
@@ -0,0 +1,17 @@
+var myArr = [name, lastname, birthdate, birthplace, imageurl];
+
+function showContent() {
+  var temp, item, a, i;
+  temp = document.getElementsByTagName("template")[0];
+  //get the div element from the template:
+  item = temp.content.querySelector("div");
+  //for each item in the array:
+  for (i = 0; i < myArr.length; i++) {
+    //Create a new node, based on the template:
+    a = document.importNode(item, true);
+    //Add data from the array:
+    a.textContent += myArr[i];
+    //append the new node wherever you like:
+    document.body.appendChild(a);
+  }
+}
