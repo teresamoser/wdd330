e3ff57ab37b95f04e388092872550a880353af34
Teresa Moser <moserte@byui.edu>
2023-03-02 18:57:38 -0700

    Template Element updates

--- prophetCard/prophets.js
+++ prophetCard/prophets.js
@@ -1,17 +1,7 @@
-var myArr = [name, lastname, birthdate, birthplace, imageurl];
+var myArr = [];
 
-function showContent() {
-  var temp, item, a, i;
-  temp = document.getElementsByTagName("template")[0];
-  //get the div element from the template:
-  item = temp.content.querySelector("div");
-  //for each item in the array:
-  for (i = 0; i < myArr.length; i++) {
-    //Create a new node, based on the template:
-    a = document.importNode(item, true);
-    //Add data from the array:
-    a.textContent += myArr[i];
-    //append the new node wherever you like:
-    document.body.appendChild(a);
-  }
+function showCard() {
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode (true);
+    document.body.appendChild(clon);
 }
