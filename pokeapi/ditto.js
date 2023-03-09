551923790d731798f3919f69d937fd7feb4e6958
Teresa Moser <moserte@byui.edu>
2023-03-08 21:44:30 -0700

    update files

--- pokeapi/ditto.js
+++ pokeapi/ditto.js
@@ -1,4 +1,4 @@
-const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const url = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 async function getPokemon(url) {
   const response = await fetch(url);
@@ -10,9 +10,20 @@ async function getPokemon(url) {
     doStuff(data);
   }
 }
+// function doStuff(data) {
+//   results = data;
+//   console.log("first: ", results);
+// }
+// getPokemon(url);
+// console.log("second: ", results);
+
 function doStuff(data) {
-  results = data;
-  console.log("first: ", results);
-}
-getPokemon(url);
-console.log("second: ", results);
+    results = data;
+    console.log('first: ', results);
+    results.results.forEach((pokemon) => {
+      const div = document.createElement('div');
+      div.textContent = pokemon.name;
+      document.querySelector('main').appendChild(div);
+        // assumes you have a <main> element in your HTML document
+    });
+  }
