956ec4fff5350285d0ca5ca448f463797eae9fe0
Teresa Moser <moserte@byui.edu>
2023-03-15 23:25:09 -0600

    updates to week 4

--- Week04/pokeman.js
+++ Week04/pokeman.js
@@ -1,4 +1,4 @@
-const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const url = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 async function getPokemon(url) {
   const response = await fetch(url);
@@ -10,9 +10,14 @@ async function getPokemon(url) {
     doStuff(data);
   }
 }
+
 function doStuff(data) {
   results = data;
-  console.log("first: ", results);
+  console.log('first: ', results);
+  results.results.forEach((pokemon) => {
+    const div = document.createElement('div');
+    div.textContent = pokemon.name;
+    document.querySelector('main').appendChild(div);
+      // assumes you have a <main> element in your HTML document
+  });
 }
-getPokemon(url);
-console.log("second: ", results);
