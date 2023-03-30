9239a5da41b0bd934d386988250a80f766e00acd
Teresa Moser <moserte@byui.edu>
2023-03-29 20:16:01 -0600

    Final Project updates

--- FinalProject/Final-Project.js
+++ FinalProject/Final-Project.js
@@ -1,4 +1,5 @@
-//select HTML elements in the document  WEATHER APP
+//WEATHER APP
+//select HTML elements in the document  
 const myTown = document.querySelector('#town')
 const myGraphic = document.querySelector('#graphic')
 const myDescription = document.querySelector('#description')
@@ -23,29 +24,29 @@ fetch(myURL)
     }
 //END OF WEATHER APP
 
-//select HTML elements in the document  WEATHER APP
-const myTown = document.querySelector('#town')
-const myGraphic = document.querySelector('#graphic')
-const myDescription = document.querySelector('#description')
-const myTemperature = document.querySelector('#temperature')
+//TEMPLATE FOR API SOLAR SYSTEM
+// const myTown = document.querySelector('#town')
+// const myGraphic = document.querySelector('#graphic')
+// const myDescription = document.querySelector('#description')
+// const myTemperature = document.querySelector('#temperature')
 
-const townName = "Rigby"
-const myKey = "595ac2cc14ab1d9e32e1f8e790eaf129"
+// const townName = "Rigby"
+// const myKey = "595ac2cc14ab1d9e32e1f8e790eaf129"
 
-const myURL = `//api.openweathermap.org/data/2.5/weather?q=${townName}&appid=${myKey}&units=imperial`;
+// const myURL = `//api.openweathermap.org/data/2.5/weather?q=${townName}&appid=${myKey}&units=imperial`;
 
-fetch(myURL)
-    .then((response) => response.json())
-    .then((data) => displayData(data));
+// fetch(myURL)
+//     .then((response) => response.json())
+//     .then((data) => displayData(data));
 
-    function displayData(data) {
-        // console.log(data)
-        myTown.innerHTML = data.name
-        myGraphic.src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-        myGraphic.alt = data.weather[0].main
-        myDescription.innerHTML = data.weather[0].description
-        myTemperature.innerHTML = `${data.main.temp}&deg;F`
-    }
+//     function displayData(data) {
+//         // console.log(data)
+//         myTown.innerHTML = data.name
+//         myGraphic.src=`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+//         myGraphic.alt = data.weather[0].main
+//         myDescription.innerHTML = data.weather[0].description
+//         myTemperature.innerHTML = `${data.main.temp}&deg;F`
+//     }
 
 // GETDATES JS 
 const d = new Date();
